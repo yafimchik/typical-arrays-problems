07c3f19daf6effe47a46f8c0106072d9ab64bfd1
yafimchik <yafimenka.jk@gmail.com>
2020-03-08 11:33:14 +0300

    init: solve task typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
+let sum
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  let result = 0;
+  let firstIter = true;
+  for (item of array){
+    if (firstIter || (result > item)) result = item;
+    firstIter = false;
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  let result = 0;
+  let firstIter = true;
+  for (item of array){
+    if (firstIter || (result < item)) result = item;
+    firstIter = false;
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length == 0) return 0;
+  let result = 0;
+  for (item of array){
+    result += item;
+  }
+  result /= array.length;
+  return result;
 }
